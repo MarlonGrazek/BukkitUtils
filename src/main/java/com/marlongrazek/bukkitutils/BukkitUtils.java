d33b3a16958e0f3080d2dedd739f8114b4f200be
Marlon Grazek <business.marlongrazek@gmail.com>
2022-01-22 15:10:38 +0100

    removePlayerExperience added

--- src/main/java/com/marlongrazek/bukkitutils/BukkitUtils.java
+++ src/main/java/com/marlongrazek/bukkitutils/BukkitUtils.java
@@ -14,11 +14,22 @@ public final class BukkitUtils {
 
     public static void addPlayerExperience(Player player, int experience) {
 
-        while (experience > player.getExpToLevel() - (int) (player.getExpToLevel() * player.getExp())) {
+        while (experience >= player.getExpToLevel() - (int) (player.getExpToLevel() * player.getExp())) {
             experience -= player.getExpToLevel() - (int) (player.getExpToLevel() * player.getExp());
             player.setLevel(player.getLevel() + 1);
         }
 
         player.setExp((float) experience / player.getExpToLevel());
+    }
+
+    public static void removePlayerExperience(Player player, int experience) {
+
+        while (experience > (int) (player.getExpToLevel() * player.getExp())) {
+            experience -= (int) (player.getExpToLevel() * player.getExp());
+            if(player.getLevel() > 0) player.setLevel(player.getLevel() - 1);
+        }
+
+        int newExp = (int) (player.getExpToLevel() * player.getExp()) - experience;
+        player.setExp((float) newExp / player.getExpToLevel());
     }
 }
