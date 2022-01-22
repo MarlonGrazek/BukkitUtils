746dd299c2ee912208f4efb239f99000faa2f80f
Marlon Grazek <business.marlongrazek@gmail.com>
2022-01-22 15:26:35 +0100

    debug

--- src/main/java/com/marlongrazek/bukkitutils/BukkitUtils.java
+++ src/main/java/com/marlongrazek/bukkitutils/BukkitUtils.java
@@ -1,5 +1,6 @@
 package com.marlongrazek.bukkitutils;
 
+import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public final class BukkitUtils {
@@ -19,17 +20,21 @@ public final class BukkitUtils {
             player.setLevel(player.getLevel() + 1);
         }
 
-        if(player.getExp() == player.getExpToLevel()) player.setLevel(player.getLevel() + 1);
+        Bukkit.broadcastMessage("ToLevel:" + player.getExpToLevel());
+        Bukkit.broadcastMessage("Left:" + (player.getExpToLevel() - (int) (player.getExpToLevel() * player.getExp())));
+        Bukkit.broadcastMessage("Progress:" + (int) (player.getExpToLevel() * player.getExp()));
+        Bukkit.broadcastMessage("NewProgress:" + (float) experience / player.getExpToLevel());
+
         player.setExp((float) experience / player.getExpToLevel());
     }
 
     public static void removePlayerExperience(Player player, int experience) {
 
-        if(player.getExp() == 0) return;
+        if (player.getExp() == 0) return;
 
         while (experience > (int) (player.getExpToLevel() * player.getExp())) {
             experience -= (int) (player.getExpToLevel() * player.getExp());
-            if(player.getLevel() > 0) player.setLevel(player.getLevel() - 1);
+            if (player.getLevel() > 0) player.setLevel(player.getLevel() - 1);
         }
 
         int newExp = (int) (player.getExpToLevel() * player.getExp()) - experience;
