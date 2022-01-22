faf153eda31700722e1ac1768134ea0326a15d61
Marlon Grazek <business.marlongrazek@gmail.com>
2022-01-22 14:54:46 +0100

    Initial commit

---
+++ src/main/java/com/marlongrazek/bukkitutils/BukkitUtils.java
@@ -0,0 +1,24 @@
+package com.marlongrazek.bukkitutils;
+
+import org.bukkit.entity.Player;
+
+public final class BukkitUtils {
+
+    public static void setPlayerExperience(Player player, int experience) {
+
+        player.setExp(0);
+        player.setLevel(0);
+
+        addPlayerExperience(player, experience);
+    }
+
+    public static void addPlayerExperience(Player player, int experience) {
+
+        while (experience > player.getExpToLevel() - (int) (player.getExpToLevel() * player.getExp())) {
+            experience -= player.getExpToLevel() - (int) (player.getExpToLevel() * player.getExp());
+            player.setLevel(player.getLevel() + 1);
+        }
+
+        player.setExp((float) experience / player.getExpToLevel());
+    }
+}
