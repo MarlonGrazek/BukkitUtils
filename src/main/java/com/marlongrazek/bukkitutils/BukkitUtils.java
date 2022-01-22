69ec33036c9762024c2c12b507558812f6d745ea
Marlon Grazek <business.marlongrazek@gmail.com>
2022-01-22 16:46:44 +0100

    fix

--- src/main/java/com/marlongrazek/bukkitutils/BukkitUtils.java
+++ src/main/java/com/marlongrazek/bukkitutils/BukkitUtils.java
@@ -1,6 +1,5 @@
 package com.marlongrazek.bukkitutils;
 
-import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
 
 public final class BukkitUtils {
@@ -15,29 +14,26 @@ public final class BukkitUtils {
 
     public static void addPlayerExperience(Player player, int experience) {
 
-        while (experience == player.getExpToLevel() - (int) (player.getExpToLevel() * player.getExp())) {
-            experience -= player.getExpToLevel() - (int) (player.getExpToLevel() * player.getExp());
+        while (experience >= player.getExpToLevel() - player.getExpToLevel() * player.getExp()) {
+            experience -= player.getExpToLevel() - player.getExpToLevel() * player.getExp();
             player.setLevel(player.getLevel() + 1);
+            player.setExp(0);
         }
 
-        Bukkit.broadcastMessage("ToLevel:" + player.getExpToLevel());
-        Bukkit.broadcastMessage("Left:" + (player.getExpToLevel() - (int) (player.getExpToLevel() * player.getExp())));
-        Bukkit.broadcastMessage("Progress:" + (int) (player.getExpToLevel() * player.getExp()));
-        Bukkit.broadcastMessage("NewProgress:" + (float) experience / player.getExpToLevel());
-
-        player.setExp((float) experience / player.getExpToLevel());
+        float newExp = experience + player.getExp() * player.getExpToLevel();
+        player.setExp(newExp / player.getExpToLevel());
     }
 
     public static void removePlayerExperience(Player player, int experience) {
 
         if (player.getExp() == 0) return;
 
-        while (experience > (int) (player.getExpToLevel() * player.getExp())) {
-            experience -= (int) (player.getExpToLevel() * player.getExp());
+        while (experience > player.getExpToLevel() * player.getExp()) {
+            experience -= player.getExpToLevel() * player.getExp();
             if (player.getLevel() > 0) player.setLevel(player.getLevel() - 1);
         }
 
-        int newExp = (int) (player.getExpToLevel() * player.getExp()) - experience;
-        player.setExp((float) newExp / player.getExpToLevel());
+        float newExp = player.getExpToLevel() * player.getExp() - experience;
+        player.setExp(newExp / player.getExpToLevel());
     }
 }
